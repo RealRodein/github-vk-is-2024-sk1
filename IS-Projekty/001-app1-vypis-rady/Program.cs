8ecbadd13301f01f996d27b0c8068045687f5eb3
Vojtěch Kupec <rodein.personal@gmail.com>
2024-10-02 09:34:16 +0200

    Udělání úlohy

--- IS-Projekty/001-app1-vypis-rady/Program.cs
+++ IS-Projekty/001-app1-vypis-rady/Program.cs
@@ -1,12 +1,58 @@
-﻿namespace _001_app1_vypis_rady
+﻿using System.Numerics;
+using System.Threading;
+
+namespace _001_app1_vypis_rady
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Boolean procceed = false;
+            String again;
+            int first;
+            int goal;
+            int step;
 
-            Console.ReadKey();
+            do
+            {
+                Console.Clear();
+
+                do
+                {
+                    Console.WriteLine("Zadej startoní číslo");
+                    first = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Zadej cílové číslo");
+                    goal = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Zadej krok");
+                    step = int.Parse(Console.ReadLine());
+
+                    procceed = (goal > first && step > 0);
+                    if (procceed == false)
+                    {
+                        Console.WriteLine("špatná kombinace čísel \n zadej klávesu pro pokračování");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                } while (procceed != true);
+
+                Console.WriteLine();
+
+                while(first <= goal)
+                {
+                    Console.WriteLine(first);
+                    first = first + step;
+                }
+
+                Console.WriteLine("Pro opakování Y/y");
+                again = Console.ReadLine();
+
+            }while (again == "Y" || again == "y");
+            
+           
+
+            
         }
     }
 }
