3a9f2a03f2488a4ee24d59c612c42464b8da50f9
Vojtěch Kupec <rodein.personal@gmail.com>
2024-10-22 23:29:54 +0200

    #3 hotovo

---
+++ IS-Projekty/003a-vypis-obrazce/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace _003_vypis_obrazce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rect_a;
+            int rect_b;
+
+
+            Console.WriteLine("Zadej hodnoty pro obdélník a,b");
+            rect_a = int.Parse(Console.ReadLine());
+            rect_b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rect_a; i++)
+            {
+                Console.Write("    ");
+                for (int j = 0; j < rect_b; j++)
+                {
+
+                    Console.Write("██");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
--- IS-Projekty/003b-vypis-obrazce/Program.cs
+++ IS-Projekty/003b-vypis-obrazce/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int tria_x;
+            int tria_y;
+            double tria_z;
+            int temp;
+
+            Console.WriteLine("Zadej hodnoty pro trojuhelník x,y");
+
+            tria_x = int.Parse(Console.ReadLine());
+            tria_y = int.Parse(Console.ReadLine());
+            tria_z = (double)tria_y / tria_x;
+
+            for (int i = 1; i <= tria_x; i++)
+            {
+                Console.Write("    ");
+                temp = (int)Math.Round(i * tria_z);
+
+                for (int j = 0; j < temp; j++)
+                {
+                    Console.Write("██");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
