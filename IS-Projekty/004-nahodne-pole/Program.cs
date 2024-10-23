5c8ee4125a64fa5723a6df368f8fdd7406e49109
Vojtěch Kupec <rodein.personal@gmail.com>
2024-10-23 10:19:29 +0200

    #4 udělaní první sub-úlohy
    tvorba licheho, sudeho pole

---
+++ IS-Projekty/004-nahodne-pole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using usefull_functions;
+
+namespace _004_nahodne_pole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadej počet n");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("zadej dolní mez");
+            int low = int.Parse(Console.ReadLine());
+            Console.WriteLine("zadej horní mez");
+            int high = int.Parse(Console.ReadLine());
+
+            int[] pole = Functions.random_array(n, low, high);
+            int[] liche_pole = Functions.random_array_odd(n, low, high);
+            int[] sude_pole = Functions.random_array_even(n, low, high);
+
+            Console.WriteLine("prvky v polich:");
+            foreach (int i in pole) { 
+            Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            foreach (int i in liche_pole)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine() ;
+            foreach (int i in sude_pole)
+            {
+                Console.Write(i + " ");
+            }
+
+
+
+        }
+    }
+}
--- IS-Projekty/usefull-functions/Program.cs
+++ IS-Projekty/usefull-functions/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 namespace usefull_functions
 {
     public class Functions
@@ -54,5 +56,67 @@ namespace usefull_functions
                 return Math.Round(x, 0);
             }
         }
+
+        public static int[] random_array(int n, int low, int high)
+        {
+            Random random = new Random();
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(low, high);
+            }
+
+            return array;
+        }
+
+        public static int[] random_array_even(int n, int low, int high)
+        {
+            Random random = new Random();
+            int temp;
+            Boolean proceed=false;
+            int[] array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                proceed = false;
+                while (!proceed)
+                {
+                    temp = random.Next(low, high);
+                    if(temp % 2 == 0)
+                    {
+                        array[i] = temp;
+                        proceed = true;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public static int[] random_array_odd(int n, int low, int high)
+        {
+            Random random = new Random();
+            int temp;
+            int[] array = new int[n];
+            Boolean proceed = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                proceed = false;
+                while (!proceed)
+                {
+                    temp = random.Next(low, high);
+                    if (temp % 2 == 1)
+                    {
+                        array[i] = temp;
+                        proceed = true;
+                    }
+                }
+            }
+            return array;
+        }
+
+
     }
 }
