96b021d5d45ef6e80af04795b2a3a5bd95ddf894
Vojtěch Kupec <rodein.personal@gmail.com>
2024-11-01 22:21:31 +0100

    #3 zhotoveni du

---
+++ IS-Projekty/003c-vypis-obrazce/Program.cs
@@ -0,0 +1,399 @@
+﻿using usefull_functions;
+
+namespace _003c_vypis_obrazce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, select;
+            bool loop = true;
+            char temp;
+
+            while(loop == true)
+            {
+                Console.Clear();
+                Console.WriteLine("~~~~~~ Vitejte ve vypisovani obrazku ~~~~~~\n");
+                Console.Write("zadejte delku obrazce: ");
+                while (!int.TryParse(Console.ReadLine(), out size))
+                {
+                    Console.Write("zadejte delku obrazce: ");
+                }
+                Console.Write("zadejte cislo obrazku.\n(aktualne zhotovene jsou 1,4,5,6,7,10,11,12,20): ");
+                while (!int.TryParse(Console.ReadLine(), out select))
+                {
+                    Console.Write("zadejte cislo obrazku: ");
+                }
+                
+                pic_out(pic_select(size,select));
+
+                Console.Write("\npokracovat? (y/Y): ");
+                temp = Console.ReadKey().KeyChar;
+                loop = (temp == 'Y' || temp == 'y' || temp == 'z' || temp == 'Z');  
+                
+            }
+
+        }
+        public static bool[,] pic_select(int size, int num)
+        {
+            bool[,] result = new bool[size, size];
+            double temp = result.GetLength(0) / 2;
+            int half = (int)Math.Round(temp);
+            int iswitch = 0;
+            int cont = 1;
+            do
+            {
+                cont = 1;
+                switch (num)
+                {
+                    case 1:
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (x == y)
+                                {
+                                    result[x, y] = true;
+                                    result[x, result.GetLength(1) - y - 1] = true;
+
+                                }
+                                else if (y == half)
+                                {
+                                    result[x, y] = true;
+                                }
+                                else if (x == half)
+                                {
+                                    result[x, y] = true;
+                                }
+                            }
+                        }
+                        break;
+                    case 4:
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (y == 0 || y == result.GetLength(0) - 1)
+                                {
+                                    result[x, y] = true;
+                                }
+                                else if (y == x)
+                                {
+                                    result[x, result.GetLength(1) - y - 1] = true;
+                                }
+                            }
+                        }
+                        break;
+                    case 5:
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (y == 0 || y == result.GetLength(0) - 1)
+                                {
+                                    result[x, y] = true;
+                                }
+                                else if (y == x)
+                                {
+                                    result[x, result.GetLength(1) - y - 1] = true;
+                                }
+                            }
+                        }
+                        result = Functions.flip_array(result);
+                        break;
+                    case 6:
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (y == 0 || y == result.GetLength(0) - 1)
+                                {
+                                    result[x, y] = true;
+                                }
+                                else if (y == x)
+                                {
+                                    result[x, result.GetLength(1) - y - 1] = true;
+                                }
+                            }
+                        }
+                        result = Functions.rotate_array_90(result);
+                        break;
+                    case 7:
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (y == 0 || y == result.GetLength(0) - 1)
+                                {
+                                    result[x, y] = true;
+                                }
+                                else if (y == x)
+                                {
+                                    result[x, result.GetLength(1) - y - 1] = true;
+                                }
+                            }
+                        }
+                        result = Functions.flip_array(Functions.rotate_array_90(result));
+                        break;
+                    case 10:
+                        iswitch = 0;
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            if (y % 2 == 1)
+                            {
+                                if (iswitch % 2 == 0)
+                                {
+                                    result[result.GetLength(0) - 1, y] = true;
+                                    iswitch++;
+                                }
+                                else
+                                {
+                                    result[0, y] = true;
+                                    iswitch++;
+                                }
+                            }
+
+
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (y % 2 == 0)
+                                {
+                                    result[x, y] = true;
+                                }
+
+                            }
+                        }
+                        break;
+                    case 11:
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            if (y % 2 == 1)
+                            {
+                                if (iswitch % 2 == 0)
+                                {
+                                    result[result.GetLength(0) - 1, y] = true;
+                                    iswitch++;
+                                }
+                                else
+                                {
+                                    result[0, y] = true;
+                                    iswitch++;
+                                }
+                            }
+
+
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (y % 2 == 0)
+                                {
+                                    result[x, y] = true;
+                                }
+
+                            }
+                        }
+                        result = Functions.rotate_array_90(result);
+                        result = Functions.flip_array(result);
+                        break;
+                    case 12:
+                        iswitch = 1;
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+                            if (iswitch % 2 == 0)
+                            {
+                                result[0, y] = true;
+                            }
+                            iswitch++;
+
+                            for (int x = 1; x < result.GetLength(0); x++)
+                            {
+                                if (result[x - 1, y] == false)
+                                {
+                                    result[x, y] = true;
+                                }
+                            }
+                        }
+                        break;
+                    case 14:
+                        for (int y = 0; y < result.GetLength(1); y++)
+                        {
+
+                            for (int x = 0; x < result.GetLength(0); x++)
+                            {
+                                if (x >= result.GetLength(0) / 2 && y < result.GetLength(1) / 2)
+                                {
+                                    result[x, y] = true;
+                                }
+                                else if (x < result.GetLength(0) / 2 && y >= result.GetLength(1) / 2)
+                                {
+                                    result[x, y] = true;
+                                }
+                            }
+                        }
+                        break;
+                    case 20:
+                        iswitch = 0;
+                        for (int y = 0; y < result.GetLength(1); y += 2)
+                        {
+                            if (iswitch % 2 == 0)
+                            {
+                                result[0, y] = true;
+                                result[1, y] = true;
+                                result[0, y + 1] = true;
+                                result[1, y + 1] = true;
+                            }
+                            iswitch++;
+
+                            for (int x = 2; x < result.GetLength(0); x += 2)
+                            {
+                                if (result[x - 2, y] == false)
+                                {
+                                    result[x, y] = true;
+                                    result[x + 1, y] = true;
+                                    result[x, y + 1] = true;
+                                    result[x + 1, y + 1] = true;
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        Console.Write("spatne cislo/nevypracovane.\nzvolte jine cislo (1,4,5,6,7,10,11,12,14,20): ");
+                        while (!int.TryParse(Console.ReadLine(), out num))
+                        {
+                            Console.Write("zadejte jine cislo obrazku: ");
+                        }
+                        cont = 0;
+                        break;
+
+                }
+            } while (cont != 1);
+            return result;
+        }
+
+        public static int[,] frame_make(int n)
+        {
+            int[,] result;
+
+            if (n < 11)
+            {
+                result = small_frame(n);
+            }
+            else
+            {
+                result = big_frame(n);
+            }
+
+            return result;
+        }
+
+        public static int[,] small_frame(int n)
+        {
+            int[,] result = new int[n + 2, n + 2];
+            result[1, 1] = -2;
+
+            for (int y = 2; y < result.GetLength(1); y++)
+            {
+                result[0, y] = y - 1;
+                result[1, y] = -3;
+                for (int x = 2; x < result.GetLength(0); x++) // < out of bounds
+                {
+                    result[x, 1] = -1;
+                    result[x, 0] = x - 1;
+
+                }
+            }
+
+            return result;
+        }
+
+        public static int[,] big_frame(int n)
+        {
+            int[,] result = new int[n + 2, n + 3];
+            result[1, 2] = -2;
+
+            for (int y = 3; y < result.GetLength(1); y++)
+            {
+                result[0, y] = y - 2;
+                result[1, y] = -3;
+
+                for (int x = 2; x < result.GetLength(0); x++)
+                {
+                    result[x, 2] = -1;
+
+                    if (x < 11)
+                    {
+                        result[x, 1] = x - 1; 
+                    }
+                    else if (x % 2 == 1)
+                    {
+                        result[x, 1] = x - 1;
+                    }
+                    else
+                    {
+                        result[x, 0] = x - 1;
+                        result[x, 1] = -4;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static void frame_out(int[,] array)
+        {
+            for (int y = 0; y < array.GetLength(1); y++)
+            {
+                for(int x = 0; x < array.GetLength(0); x++)
+                {
+                    switch (array[x,y])
+                    {
+                        case 0: 
+                            Console.Write("  ");
+                            break;
+                        case -1:
+                            Console.Write("══");
+                            break;
+                        case -2: 
+                            Console.Write("╔═");
+                            break;
+                        case -3:
+                            Console.Write("║ ");
+                            break;
+                        case -4:
+                            Console.Write("↓ ");
+                            break;
+                        default:
+                            if (array[x, y] < 10)
+                            {
+                                Console.Write(array[x, y] + " ");
+                            }
+                            else
+                            { 
+                                Console.Write(array[x, y]);
+                            } 
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+        public static void pic_out(bool[,] array)
+        {
+            Console.Clear();
+            Console.WriteLine("~~~~~~ Vitejte ve vypisovani obrazku ~~~~~~\n");
+
+            frame_out(frame_make(array.GetLength(0)));
+            if(array.GetLength(0) < 11)
+            {
+                Functions.array_bool_out(array,4,4);
+            }
+            else
+            {
+                Functions.array_bool_out(array,4, 5);
+            }
+        }
+    }
+}
--- IS-Projekty/usefull-functions/Program.cs
+++ IS-Projekty/usefull-functions/Program.cs
@@ -5,8 +5,13 @@ namespace usefull_functions
 {
     public class Functions
     {
-        public static void Main(String[] args) { }
+        public static void Main(String[] args) {
+            // T E S T I N G   Z O N E
 
+
+
+        }
+        #region UTILITY
         public static Boolean is_parseable(string S)
         {
             Boolean result = true;
@@ -56,7 +61,9 @@ namespace usefull_functions
                 return Math.Round(x, 0);
             }
         }
+        #endregion
 
+        #region 1D-ARRAYS
         public static int[] random_array(int n, int low, int high)
         {
             Random random = new Random();
@@ -75,7 +82,7 @@ namespace usefull_functions
         {
             Random random = new Random();
             int temp;
-            Boolean proceed=false;
+            Boolean proceed = false;
             int[] array = new int[n];
 
             for (int i = 0; i < array.Length; i++)
@@ -84,7 +91,7 @@ namespace usefull_functions
                 while (!proceed)
                 {
                     temp = random.Next(low, high);
-                    if(temp % 2 == 0)
+                    if (temp % 2 == 0)
                     {
                         array[i] = temp;
                         proceed = true;
@@ -116,7 +123,85 @@ namespace usefull_functions
             }
             return array;
         }
+        #endregion
 
+        #region ARRAYS-BOOL
+        public static void array_bool_out(bool[,] array)
+        {
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                for (int y = 0; y < array.GetLength(1); y++)
+                {
+                    if (array[x, y] == true)
+                    {
+                        Console.Write("██");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void array_bool_out(bool[,] array, int i,int j)
+        {
+            Console.SetCursorPosition(i, j);
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    if (array[x, y] == true)
+                    {
+                        Console.Write("██");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                j++;
+                Console.SetCursorPosition(i,j);
+            }
+        }
+        public static bool[,] rotate_array_90(bool[,] array)
+        {
+            bool[,] result = new bool[array.GetLength(0), array.GetLength(1)];
+            for (int y = 0; y < array.GetLength(1); y++)
+            {
+                for (int x = 0; x < array.GetLength(0); x++)
+                {
+                    result[y, x] = array[x, array.GetLength(0) -y - 1];
+                }
+            }
+            return result;
+        }
 
+        public static bool[,] rotate_array_180(bool[,] array)
+        {
+            bool[,] result = rotate_array_90(rotate_array_90(array));
+            return result;
+        }
+
+        public static bool[,] rotate_array_270(bool[,] array)
+        {
+            bool[,] result = rotate_array_90(rotate_array_180(array));
+            return result;
+        }
+
+        public static bool[,] flip_array(bool[,] array)
+        {
+            bool[,] result = new bool[array.GetLength(0), array.GetLength(1)];
+
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    result[y, x] = array[array.GetLength(0) - y - 1, x];
+                }
+            }
+            return result;
+        }
+        #endregion
     }
 }
