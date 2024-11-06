e0d79b6d9ad3df7a4964cb46f8492e91bb315e32
Vojtěch Kupec <rodein.personal@gmail.com>
2024-11-06 10:53:07 +0100

    #7 hotovo, obdrzen+

---
+++ IS-Projekty/006-analyza-textu/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _006_analyza_textu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte text:");
+            String to_analyze = Console.ReadLine();
+            int samohlasky = 0;
+            int souhlasky = 0;
+            int ostatnich = 0;
+
+            foreach (char c in to_analyze) {
+                if ("eyuioaěáíéúůEYUIOAÁÝÍÉÚŮ".Contains(c)) 
+                {
+                    samohlasky++;
+                }
+                else if ("qwrtpsdfghjklxcvbnmščřžQWRTPSDFGHJKLZXCVBNMŠČŘŽŤŇ".Contains(c))
+                {
+                    souhlasky++;
+                }
+                else
+                {
+                    ostatnich++;
+                }
+            }
+
+            Console.WriteLine("pocet samohlasek: {0}\npocet souhlasek: {1}\npocet ostatnich: {2}", samohlasky, souhlasky, ostatnich);
+            Console.ReadKey();
+
+        }
+    }
+}
