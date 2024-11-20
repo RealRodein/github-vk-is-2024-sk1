a84df79244e86b97da72a28b546ec2f35d02abd7
Vojtěch Kupec <rodein.personal@gmail.com>
2024-11-20 21:20:37 +0100

    #12 hotovo

---
+++ IS-Projekty/011a-intervaly/Program.cs
@@ -0,0 +1,48 @@
+﻿using usefull_functions;
+
+namespace _011a_intervaly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte pozadovanou delku pole, dm, hm:");
+            int n = int.Parse(Console.ReadLine());
+            int dm = int.Parse(Console.ReadLine());
+            int hm = int.Parse(Console.ReadLine());
+            double quater = ((hm + 1) - dm) / 4;
+            int count1 = 0, count2 = 0, count3 = 0, count4 = 0;
+
+            int[] array = Functions.random_array(n, dm, hm);
+
+            Functions.array_out(array);
+            foreach (int i in array)
+            {
+                if (i <= quater)
+                {
+                    count1++;
+                }
+                else if (i <= quater + quater)
+                {
+                    count2++;
+                }
+                else if (i <= quater + quater + quater)
+                {
+                    count3++;
+                }
+                else if (i <= hm)
+                {
+                    count4++;
+                }
+            }
+
+            Console.WriteLine("v intervalu <{0}-{1}>: {2}", dm, quater, count1);
+            Console.WriteLine("v intervalu ({0}-{1}>: {2}", quater, quater + quater, count2);
+            Console.WriteLine("v intervalu ({0}-{1}>: {2}", quater + quater, quater + quater + quater, count3);
+            Console.WriteLine("v intervalu ({0}-{1}>: {2}", quater + quater + quater, hm, count4);
+
+
+
+        }
+    }
+}
---
+++ IS-Projekty/011b-intervaly/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+using usefull_functions;
+
+namespace _011b_intervaly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte pozadovanou delku pole, dm, hm, pocet intervalu:");
+            int n = int.Parse(Console.ReadLine());
+            int dm = int.Parse(Console.ReadLine());
+            int hm = int.Parse(Console.ReadLine());
+            int pi = int.Parse(Console.ReadLine());
+
+            int[] array = Functions.random_array(n, dm, hm);
+
+            int[] intervals = new int[pi+1];
+            intervals[0] = dm;
+            int[] count = new int[pi];
+            
+            double mez = ((hm + 1) - dm) / pi;
+
+
+            for(int i = 1; i < intervals.Length; i++)
+            {
+                intervals[i] = (int)(i * mez);
+            }
+            
+            Console.WriteLine();
+            Functions.array_out(array);
+            Console.WriteLine();
+
+            int a = 0, b = 0;
+            Array.Sort(array);
+            while (a < array.Length && b < intervals.Length - 1)
+            {
+                if (array[a] <= intervals[b + 1])
+                {
+                    count[b]++;
+                    a++;
+                }
+                else
+                {
+                    b++;
+                }
+            }
+            while (a < array.Length)
+            {
+                count[intervals.Length - 2]++;
+                a++;
+            }
+
+            for (int i = 0; i < intervals.Length-1; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("v intervalu <{0}-{1}>: {2}", intervals[i], intervals[i+1], count[i]);
+                }
+                else
+                {
+                    Console.WriteLine("v intervalu <{0}-{1}>: {2}", intervals[i]+1, intervals[i + 1], count[i]);
+                }
+                   
+            }
+
+            Console.WriteLine("\n" + Functions.array_sum(count));
+
+        }
+    }
+}
