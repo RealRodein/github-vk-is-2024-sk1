0be208970a213d1ba3e68565082c81fe9a2dd799
Vojtěch Kupec <rodein.personal@gmail.com>
2024-11-20 21:21:23 +0100

    #13 hotovo

---
+++ IS-Projekty/012-aritmeticka-posloupnost/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _012_aritmeticka_posloupnost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("pocet clenu: ");
+            int count = int.Parse(Console.ReadLine());
+
+            Console.Write("difference: ");
+            int diference = int.Parse(Console.ReadLine());
+
+            Console.Write("prvni clen: ");
+            int first = int.Parse(Console.ReadLine());
+
+            int total = 0;
+            Console.Write("posloupnost: ");
+            for (int i = count; i != 0; i--)
+            {
+                Console.Write(first + " ");
+                total += first;
+                first += diference;
+            }
+
+            // Výpis součtu
+            Console.WriteLine("\ncelkem: " + total);
+        }
+    }
+}
