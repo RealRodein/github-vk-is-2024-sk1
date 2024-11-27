e4255b76bb9811c53c7ac9c669f4926c56cd74fa
Vojtěch Kupec <rodein.personal@gmail.com>
2024-11-27 09:05:01 +0100

    #14 neco udelane

---
+++ IS-Projekty/013-prevod-soustav/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _013_prevod_soustav
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("zadej cislo v desitkove: ");
+            int decimal_num = int.Parse(Console.ReadLine());
+            Console.Write("zadej cilovou soustavu (max.:16): ");
+            int base_num = int.Parse(Console.ReadLine());
+
+            string vysledek = dec_to_base(decimal_num, base_num);
+            Console.WriteLine("Výsledek v cílové soustavě: " + vysledek);
+        }
+
+        static string dec_to_base(int num, int @base)
+        {
+            if (num == 0) return "0";
+
+            string result = "";
+            while (num > 0)
+            {
+                int temp = num % @base;
+                result = (temp >= 10 ? ((char)('A' + temp - 10)).ToString() : temp.ToString()) + result;
+                num /= @base;
+            }
+            return result;
+        }
+    }
+}
