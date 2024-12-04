b69109a1a05a01dca7735e4fd51b011b6f112770
Vojtěch Kupec <vojtech.kupec@tul.cz>
2024-12-04 10:15:13 +0100

    #15 hotovo

---
+++ IS-Projekty/015-nsd-nsn/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _015_nsd_nsn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong i = 0;
+            ulong nsd = 0;
+            ulong nsn = 0;
+            Console.Write("Zadej x: ");
+            ulong x = ulong.Parse(Console.ReadLine());
+            Console.Write("Zadej y: ");
+            ulong y = ulong.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                i = y;
+            }
+            else
+            {
+                i = x;
+            }
+
+            for (ulong j = i; j > 0; j--) {
+                if (x % j == 0 && y % j == 0) { 
+                    nsd = j;
+                    nsn = (x * y) / nsd;
+                    break;
+                }
+            }
+
+            Console.WriteLine("nsd: {0} nsn: {1}", nsd, nsn);
+        }
+    }
+}
